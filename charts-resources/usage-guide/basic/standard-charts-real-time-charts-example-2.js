fc938c9f862fdee1f18c5b4c9c52588c93286fa6
Suvradip Saha <suvradip@fusioncharts.com>
2018-02-15 21:33:22 +0530

    changes

--- charts-resources/usage-guide/basic/standard-charts-real-time-charts-example-2.js
+++ charts-resources/usage-guide/basic/standard-charts-real-time-charts-example-2.js
@@ -59,13 +59,14 @@
     },
     "events": {
         "initialized": function(e) {
+			var chartRef = e.sender;
             function addLeadingZero(num) {
                 return (num <= 9) ? ("0" + num) : num;
             }
 
             function updateData() {
                 // Get reference to the chart using its ID
-                var chartRef = FusionCharts("stockRealTimeChart"),
+				// var chartRef = FusionCharts("stockRealTimeChart"),
                     // We need to create a querystring format incremental update, containing
                     // label in hh:mm:ss format
                     // and a value (random).
