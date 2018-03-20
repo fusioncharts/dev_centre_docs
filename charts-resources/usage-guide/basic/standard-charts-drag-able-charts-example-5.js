2786955de98b5261dc020da66ae7627a103faf5d
Dishank Tiwari <dishank@fusioncharts.com>
2018-03-20 13:24:49 +0530

    samples updated for drag node chart

--- charts-resources/usage-guide/basic/standard-charts-drag-able-charts-example-5.js
+++ charts-resources/usage-guide/basic/standard-charts-drag-able-charts-example-5.js
@@ -64,7 +64,7 @@
                 tdStyle = "background-color: #fff;color: #6baa01; border: 1px solid #6baa01; padding: 10px 5px;min-width: 70px;text-align: center",
                 strHTML = "";
             tableCont.setAttribute('id', 'tableCont');
-            tableContsetAttribute('style', 'padding-left: 10px; width: 470px;');;
+            tableCont.setAttribute('style', 'padding-left: 10px; width: 470px;');;
             strHTML = "<table style='margin: 25px auto;border-collapse: collapse;border: 1px solid;border-bottom: 2px solid #6baa01'><tr><td style='border: 1px solid #fff;padding: 10px 5px;min-width: 70px;text-align: center'></td><th style='" + thStyle + "'>Poultry</th><th style='" + thStyle + "'>Rice</th><th style='" + thStyle + "'>Peanut Butter</th><th style='" + thStyle + "'>Salmon</th><th style='" + thStyle + "'>Cereal</th></tr><tr><td style='background-color: #6baa01;color: #fff; border: 1px solid #6baa01; padding: 10px 5px;min-width: 70px;text-align: center'>Estimated Demand</td><td id='est-1' style='" + tdStyle + "'>19000</td><td id='est-2' style='" + tdStyle + "'>16500</td><td id='est-3' style='" + tdStyle + "'>14300</td><td id='est-4' style='" + tdStyle + "'>10000</td><td id='est-5' style='" + tdStyle + "'>9800</td></tr></table>";
             tableCont.innerHTML = strHTML;
 
@@ -73,7 +73,6 @@
         'dataplotDragEnd': function(evt, arg) {
             var dtIndx = arg && arg.dataIndex,
                 val = arg && parseInt(arg.endValue, 10);
-
             document.getElementById('est-' + dtIndx).innerHTML = val;
         },
         'dataRestored': function(evtObj) {
