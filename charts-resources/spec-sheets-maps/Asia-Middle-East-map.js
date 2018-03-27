43516e71c48ae1e9ed4c6e58ffcdeb16e9ce190a
Dishank Tiwari <dishank@fusioncharts.com>
2018-03-27 15:27:08 +0530

    spec sheets and marker sheets updated

---
+++ charts-resources/spec-sheets-maps/Asia-Middle-East-map.js
@@ -0,0 +1,37 @@
+{
+        type: 'maps/asia3',
+        renderAt: 'chart-container',
+        width: '700',
+        height: '500',
+        dataFormat: 'json',
+        dataSource: {
+            "chart": {
+                "animation": "0",
+                "showbevel": "0",
+                "usehovercolor": "1",
+                "canvasbordercolor": "FFFFFF",
+                "bordercolor": "FFFFFF",
+                "showlegend": "1",
+                "showshadow": "0",
+                "legendposition": "BOTTOM",
+                "legendborderalpha": "0",
+                "legendbordercolor": "ffffff",
+                "legendallowdrag": "0",
+                "legendshadow": "0",
+                "caption": "Website Visits for the month of Jan 2014",
+                "connectorcolor": "000000",
+                "fillalpha": "80",
+                "hovercolor": "CCCCCC",
+                "showborder": 0
+            },
+            "colorrange": {
+                "minvalue": "0",
+                "startlabel": "Low",
+                "endlabel": "High",
+                "code": "e44a00",
+                "gradient": "1",
+                "color": [{"maxvalue": "2500", "code": "f8bd19"}, {"maxvalue": "5000", "code": "6baa01"}]
+            },
+            "data": [{"id":"002","value":1340},{"id":"003","value":931},{"id":"005","value":292},{"id":"006","value":1982},{"id":"007","value":2609},{"id":"008","value":4526},{"id":"009","value":2406},{"id":"010","value":1669},{"id":"012","value":1073},{"id":"013","value":3204},{"id":"050","value":2775},{"id":"014","value":4045},{"id":"015","value":3165},{"id":"019","value":4500},{"id":"021","value":3698},{"id":"022","value":472},{"id":"023","value":3836},{"id":"026","value":1817},{"id":"051","value":402},{"id":"028","value":4417},{"id":"030","value":695},{"id":"031","value":1610},{"id":"034","value":771},{"id":"036","value":4084},{"id":"038","value":248},{"id":"039","value":2094},{"id":"049","value":1199},{"id":"042","value":2414},{"id":"047","value":1100}]
+        }
+    }
