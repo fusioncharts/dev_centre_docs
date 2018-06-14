2e070dc076b89d4ca67c533cb169d52a0abd24b6
Dishank Tiwari <dishank@fusioncharts.com>
2018-06-14 14:13:38 +0530

    description and sample updated for inverse y-axis chart

--- charts-resources/usage-guide/basic/standard-charts-inverse-charts-example-1.js
+++ charts-resources/usage-guide/basic/standard-charts-inverse-charts-example-1.js
@@ -55,41 +55,5 @@
                 "value": "3.2"
             }]
         }]
-    },
-    events: {
-        'beforeRender': function(evt, args) {
-            // creating div for controllers
-            var controllers = document.createElement('div'),
-                gaugeRef = evt.sender;
-            controllers.setAttribute('id', 'controllers');
-            // Create radio buttons inside div
-
-            controllers.innerHTML = "<input name='btnGrp' id='inversemscolumn2d' type='radio' value='inversemscolumn2d' checked> Inverse Column 2D Chart</input><input name='btnGrp' id='mscolumn2d' type='radio' value='mscolumn2d'>Column 2D Chart</input>";
-            args.container.appendChild(controllers);
-            // set css style for controllers div
-            controllers.style.cssText = 'width:500px;text-align: center';
-
-            // set css styles for "input" elements in controls div
-            var input = controllers.getElementsByTagName('input');
-            for (i = 0; i < input.length; i++) {
-                if (input[i].getAttribute('type') == 'text') input[i].style.cssText = 'max-width: 50px;';
-            }
-            // select radio-button according to current type
-            document.getElementById(FusionCharts.items['time-chart'].chartType()).setAttribute('checked', 'true');
-        },
-        'renderComplete': function() {
-            var radio = document.getElementsByTagName('input'),
-                timeChart = FusionCharts.items['time-chart'],
-                radElem, val;
-            for (var i = 0; i < radio.length; i++) {
-                radElem = radio[i];
-                if (radElem.type === 'radio') {
-                    radElem.onclick = function() {
-                        val = this.getAttribute('value');
-                        val && timeChart.chartType(val);
-                    };
-                }
-            }
-        }
     }
 }
