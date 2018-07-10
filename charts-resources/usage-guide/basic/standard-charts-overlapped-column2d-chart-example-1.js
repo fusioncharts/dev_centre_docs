2ffb5a0e43d2d34e382fa882f796050d8dea7b79
Dishank Tiwari <dishank@fusioncharts.com>
2018-07-10 14:22:30 +0530

    samples added for overlapped chart

---
+++ charts-resources/usage-guide/basic/standard-charts-overlapped-column2d-chart-example-1.js
@@ -0,0 +1,118 @@
+{
+    type: 'overlappedcolumn2d',
+    renderAt: 'chart-container',
+    id: "myChart",
+    width: '500',
+    height: '300',
+    dataFormat: 'json',
+    dataSource: {
+        "chart": {
+            "caption": "Comparison of Quarterly Revenue",
+            "xAxisname": "Quarter",
+            "yAxisName": "Revenues (In USD)",
+            "numberPrefix": "$",
+            "plotFillAlpha": "80",
+            "paletteColors": "#0075c2,#1aaf5d",
+            "baseFontColor": "#333333",
+            "baseFont": "Helvetica Neue,Arial",
+            "captionFontSize": "14",
+            "subcaptionFontSize": "14",
+            "subcaptionFontBold": "0",
+            "showBorder": "0",
+            "bgColor": "#ffffff",
+            "showShadow": "0",
+            "canvasBgColor": "#ffffff",
+            "canvasBorderAlpha": "0",
+            "divlineAlpha": "100",
+            "divlineColor": "#999999",
+            "divlineThickness": "1",
+            "divLineIsDashed": "1",
+            "divLineDashLen": "1",
+            "divLineGapLen": "1",
+            "usePlotGradientColor": "0",
+            "showplotborder": "0",
+            "valueFontColor": "#ffffff",
+            "placeValuesInside": "1",
+            "showHoverEffect": "1",
+            "rotateValues": "1",
+            "showXAxisLine": "1",
+            "xAxisLineThickness": "1",
+            "xAxisLineColor": "#999999",
+            "showAlternateHGridColor": "0",
+            "legendBgAlpha": "0",
+            "legendBorderAlpha": "0",
+            "legendShadow": "0",
+            "legendItemFontSize": "10",
+            "legendItemFontColor": "#666666"
+        },
+        "categories": [{
+            "category": [{
+                    "label": "Q1"
+                },
+                {
+                    "label": "Q2"
+                },
+                {
+                    "label": "Q3"
+                },
+                {
+                    "label": "Q4"
+                }
+            ]
+        }],
+        "dataset": [{
+                "seriesname": "Previous Year",
+                "data": [{
+                        "value": "10000"
+                    },
+                    {
+                        "value": "11500"
+                    },
+                    {
+                        "value": "12500"
+                    },
+                    {
+                        "value": "15000"
+                    }
+                ]
+            },
+            {
+                "seriesname": "Current Year",
+                "data": [{
+                        "value": "25400"
+                    },
+                    {
+                        "value": "29800"
+                    },
+                    {
+                        "value": "21800"
+                    },
+                    {
+                        "value": "26800"
+                    }
+                ]
+            }
+        ],
+        "trendlines": [{
+            "line": [{
+                    "startvalue": "12250",
+                    "color": "#0075c2",
+                    "displayvalue": "Previous{br}Average",
+                    "valueOnRight": "1",
+                    "thickness": "1",
+                    "showBelow": "1",
+                    "tooltext": "Previous year quarterly target  : $13.5K"
+                },
+                {
+                    "startvalue": "25950",
+                    "color": "#1aaf5d",
+                    "displayvalue": "Current{br}Average",
+                    "valueOnRight": "1",
+                    "thickness": "1",
+                    "showBelow": "1",
+                    "tooltext": "Current year quarterly target  : $23K"
+                }
+            ]
+        }]
+    }
+}
