488ddf892198ce3c4157b2d93722dfbea93937ca
Dishank Tiwari <dishank@fusioncharts.com>
2018-08-14 12:28:23 +0530

    tick marks updated

--- charts-resources/usage-guide/basic/chart-configurations-tick-marks-example-6.js
+++ charts-resources/usage-guide/basic/chart-configurations-tick-marks-example-6.js
@@ -1,37 +1,40 @@
 {
-    type: 'hled',
+    type: 'vled',
     renderAt: 'chart-container',
-    
-    width: '500',
-    height: '150',
+    id: 'myChart',
+    width: '170',
+    height: '500',
     dataFormat: 'json',
     dataSource: {
         "chart": {
-            "caption": "Harry's Supermart - Bakersfield Central",
-            "subCaption": "Footfall vs Conversion ratio",
+            "caption": "Fuel Level Indicator",
+            "lowerLimit": "0",
+            "upperLimit": "100",
+            "lowerLimitDisplay": "Empty",
+            "upperLimitDisplay": "Full",
             "numberSuffix": "%",
-            //Configuring ticks to appear above gauge
-            "ticksOnRight": "1",
+            "showValue": "1",
+            "valueFontSize": "12",
+            "showhovereffect": "1",
+            "ticksOnRight": "0",
+            "chartBottomMargin": "20",
             "theme": "fusion"
         },
-        "colorrange": {
+        "colorRange": {
             "color": [{
-                "minvalue": "100",
-                "maxvalue": "76",
-                "label": "Bad",
-                "code": "#00FF00"
+                "minValue": "0",
+                "maxValue": "45",
+                "code": "#e44a00"
             }, {
-                "minvalue": "75",
-                "maxvalue": "36",
-                "label": "Average",
-                "code": "#FFFF00"
+                "minValue": "45",
+                "maxValue": "75",
+                "code": "#f8bd19"
             }, {
-                "minvalue": "0",
-                "maxvalue": "35",
-                "label": "Good",
-                "code": "#FF0000"
+                "minValue": "75",
+                "maxValue": "100",
+                "code": "#6baa01"
             }]
         },
-        "value": "62"
+        "value": "92"
     }
 }
