21c63d0463a8d27fdd64cd9cb3bfb2a646daa51c
Dishank Tiwari <dishank@fusioncharts.com>
2018-08-14 15:30:54 +0530

    themes page updated

--- charts-resources/usage-guide/basic/themes-introduction-to-themes-example-1.js
+++ charts-resources/usage-guide/basic/themes-introduction-to-themes-example-1.js
@@ -52,57 +52,44 @@
         }]
     },
     events: {
-        'beforeRender': function(evt, args) {
-            // creating div for theme controllers
+      'beforeRender': function(evt, args) {
+        // creating div for theme controllers
             var scriptElem, controllers = document.createElement('div'),
-                themeCollection = {
-                    'fusion': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.fusion.js',
-                    'gammel': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.gammel.js',
-                    'candy': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.candy.js',
-                    'zune': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.zune.js',
-                    'ocean': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.ocean.js',
-                    'carbon': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.carbon.js'
-                };
+            themeCollection = {
+                'fusion': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.fusion.js',
+                'gammel': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.gammel.js',
+                'candy': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.candy.js',
+                'zune': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.zune.js',
+                'ocean': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.ocean.js',
+                'carbon': 'https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.carbon.js'
+            };
             for (var theme in themeCollection) {
                 scriptElem = document.createElement('script');
                 scriptElem.setAttribute('type', 'text/javascript');
                 scriptElem.setAttribute('src', themeCollection[theme]);
                 scriptElem.async = false;
                 args.container.parentNode.insertBefore(scriptElem, args.container.nextSibling);
-
             }
             // form radio buttons inside div
-            controllers.innerHTML = '<label><input type="radio" name="theme-options" value="fusion"> Fusion</label><label><input type="radio" name="theme-options" value="gammel"> Gammel</label><label><input type="radio" name="theme-options" value="candy"> Candy</label><label><input type="radio" name="theme-options" id="fusion-chckbx" value="zune"> Zune</label><label><input type="radio" name="theme-options" value="ocean"> Ocean</label><label><input type="radio" name="theme-options" value="carbon"> Carbon</label><label><input type="button" value="Apply Theme" id="set-theme"></label>';
+            controllers.innerHTML = '<form name="radioBtn"><label><input type="radio" name="themeOptions" value="fusion"> Fusion</label><label><input type="radio" name="themeOptions" value="gammel"> Gammel</label><label><input type="radio" name="themeOptions" value="candy"> Candy</label><label><input type="radio" name="themeOptions" id="fint-chckbx" value="zune"> Zune</label><label><input type="radio" name="themeOptions" value="ocean"> Ocean</label><label><input type="radio" name="themeOptions" value="carbon"> Carbon</label></form>';
             controllers.setAttribute('id', 'controllers');
 
             // setting css styles for controllers div
-            controllers.style.cssText = "font-family:'Helvetica Neue', Arial; font-size:14px; margin-left:40px;";
+            controllers.style.cssText = "font-family:'Helvetica Neue', Arial; font-size:14px; margin-left:30px;";
 
             // setting css styles for labels inside controllers div
             var labels = controllers.getElementsByTagName('label');
             for (i = 0; i < labels.length; i++) {
-                labels[i].style.cssText = 'padding: 0 5px !important;';
+                labels[i].style.cssText = 'padding: 0 11px !important;';
             }
-
             args.container.appendChild(controllers);
-            //Button click handler
-            document.getElementById("set-theme").onclick = function() {
-                //Get name of the selected theme from radio
-                var radios = document.getElementsByName("theme-options"),
-                    len = radios && radios.length,
-                    isSelected, radElem, i;
-
-                for (i = 0; i < len; i += 1) {
-                    radElem = radios[i];
-                    isSelected = radElem && radios[i].checked;
-
-                    if (isSelected) {
-                        //Update chart theme
-                        FusionCharts.items["rev-fc-chart"].setChartAttribute('theme', radElem.value);
-                    }
-                }
+            
+            var rad = document.radioBtn.themeOptions;
+            for (var i = 0; i < rad.length; i++) {
+                rad[i].onclick = function() {
+                    FusionCharts.items["rev-fc-chart"].setChartAttribute('theme', this.value);
+                };
             }
-
         }
     }
 }
