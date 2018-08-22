a062d7d8b63012e4513658a6731a820dc8546dc1
Dishank Tiwari <dishank@fusioncharts.com>
2018-08-22 14:32:46 +0530

    line sample updated

--- charts-resources/chart-attributes/xt-attr-line.js
+++ charts-resources/chart-attributes/xt-attr-line.js
@@ -10,30 +10,8 @@
             "subCaption": "Last week",
             "xAxisName": "Day",
             "yAxisName": "No. of Visitors",
-
-            //Cosmetics
             "lineThickness": "2",
-            "paletteColors": "#0075c2",
-            "baseFontColor": "#333333",
-            "baseFont": "Helvetica Neue,Arial",
-            "captionFontSize": "14",
-            "subcaptionFontSize": "14",
-            "subcaptionFontBold": "0",
-            "showBorder": "0",
-            "bgColor": "#ffffff",
-            "showShadow": "0",
-            "canvasBgColor": "#ffffff",
-            "canvasBorderAlpha": "0",
-            "divlineAlpha": "100",
-            "divlineColor": "#999999",
-            "divlineThickness": "1",
-            "divLineDashed": "1",
-            "divLineDashLen": "1",
-            "showXAxisLine": "1",
-            "xAxisLineThickness": "1",
-            "xAxisLineColor": "#999999",
-            "showAlternateHGridColor": "0",
-
+            "theme": "fusion"
         },
         "data": [{
             "label": "Mon",
