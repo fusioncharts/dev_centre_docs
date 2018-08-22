a01b036c653beaa47fb21d37b1d9c9dd5641321e
Dishank Tiwari <dishank@fusioncharts.com>
2018-08-22 16:05:13 +0530

    area2d sample updated

--- charts-resources/chart-attributes/xt-attr-msstackedcolumn2dsplinedy.js
+++ charts-resources/chart-attributes/xt-attr-msstackedcolumn2dsplinedy.js
@@ -11,27 +11,7 @@
             "xAxisName": "Day",
             "yAxisName": "Sales (In USD)",
             "numberPrefix": "$",
-            "paletteColors": "#0075c2",
-            "bgColor": "#ffffff",
-            "showBorder": "0",
-            "showCanvasBorder": "0",
-            "plotBorderAlpha": "10",
-            "usePlotGradientColor": "0",
-            "plotFillAlpha": "50",
-            "showXAxisLine": "1",
-            "axisLineAlpha": "25",
-            "divLineAlpha": "10",
-            "showValues": "1",
-            "showAlternateHGridColor": "0",
-            "captionFontSize": "14",
-            "subcaptionFontSize": "14",
-            "subcaptionFontBold": "0",
-            "toolTipColor": "#ffffff",
-            "toolTipBorderThickness": "0",
-            "toolTipBgColor": "#000000",
-            "toolTipBgAlpha": "80",
-            "toolTipBorderRadius": "2",
-            "toolTipPadding": "5"
+            "theme": "fusion"
         },
 
         "data": [{
