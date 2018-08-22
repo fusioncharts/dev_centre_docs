66027608cea13d5ad958dbc48ec70fc20382c49e
Dishank Tiwari <dishank@fusioncharts.com>
2018-08-22 16:41:13 +0530

    pareto3d sample updated

--- charts-resources/chart-attributes/xt-attr-pareto3d.js
+++ charts-resources/chart-attributes/xt-attr-pareto3d.js
@@ -8,24 +8,10 @@
         "chart": {
             "caption": "Employee late arrivals by reported cause",
             "subCaption": "Last month",
-            "paletteColors": "#0075c2",
-            "lineColor": "#1aaf5d",
             "xAxisName": "Reported Cause",
             "pYAxisName": "No. of Occurrence",
             "sYAxisname": "Cumulative Percentage",
-            "bgColor": "#ffffff",
-            "borderAlpha": "20",
-            "showCanvasBorder": "0",
-            "usePlotGradientColor": "0",
-            "plotBorderAlpha": "10",
-            "showHoverEffect": "1",
-            "showValues": "0",
-            "showXAxisLine": "1",
-            "xAxisLineColor": "#999999",
-            "divlineColor": "#999999",
-            "showAlternateHGridColor": "0",
-            "subcaptionFontBold": "0",
-            "subcaptionFontSize": "14"
+            "theme": "fusion"
         },
         "data": [{
             "label": "Traffic",
