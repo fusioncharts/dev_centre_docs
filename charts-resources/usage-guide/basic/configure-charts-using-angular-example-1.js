00c2f55b40b126f665e261b2e32548acc8d8e5b5
Dishank Tiwari <dishank@fusioncharts.com>
2018-08-29 14:37:37 +0530

    vue chart update

--- charts-resources/usage-guide/basic/configure-charts-using-angular-example-1.js
+++ charts-resources/usage-guide/basic/configure-charts-using-angular-example-1.js
@@ -42,52 +42,24 @@
         }]
     },
     events: {
-        "beforeRender": function(evt, args) {
-            var controls = document.createElement('div'),
-                chartRef = evt.sender;
-            updateData = function() {
-                var newData = {
-                    // Chart Configuration
-                    "chart": {
-                        "caption": "Countries With Most Oil Reserves [2017-18]",
-                        "subCaption": "In MMbbl = One Million barrels",
-                        "xAxisName": "Country",
-                        "yAxisName": "Reserves (MMbbl)",
-                        "numberSuffix": "K",
-                        "theme": "fusion",
-                    },
-                    // Chart Data
-                    "data": [{
-                        "label": "Venezuela",
-                        "value": "200"
-                    }, {
-                        "label": "Saudi",
-                        "value": "260"
-                    }, {
-                        "label": "Canada",
-                        "value": "210"
-                    }, {
-                        "label": "Iran",
-                        "value": "190"
-                    }, {
-                        "label": "Russia",
-                        "value": "115"
-                    }, {
-                        "label": "UAE",
-                        "value": "120"
-                    }, {
-                        "label": "US",
-                        "value": "30"
-                    }, {
-                        "label": "China",
-                        "value": "30"
-                    }]
-                };
-                chartRef.setJSONData(newData);
-            };
-            controls.innerHTML = '<button onClick="updateData()" >Update chart data</button>';
-            controls.style.cssText = 'text-align: center; width: 400px; padding-left: 200px;';
-            args.container.appendChild(controls);
-        }
+        // Updates the chart data
+        updateData: function() {
+            const data = Object.assign({}, this.dataSource); //clones data
+            data.data[2].label = 'This Label is Updated';
+            data.data[2].value = this.getRandomNumber();
+
+            data.data[3].label = 'This is updated as well';
+            data.data[3].value = this.getRandomNumber();
+            this.dataSource = data;
+        },
+        // Generates a random number between min and max
+        getRandomNumber: function() {
+            var max = 300,
+                min = 50;
+            return Math.round(((max - min) * Math.random()) + min);
+        },
+        controls.innerHTML = '<button onClick="updateData()" >Update chart data</button>';
+        controls.style.cssText = 'text-align: center; width: 400px; padding-left: 200px;';
+        args.container.appendChild(controls);
     }
 }
