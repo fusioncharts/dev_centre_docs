9f9437f43367188c1e5edc79aee13b450ad862ef
Dishank Tiwari <dishank@fusioncharts.com>
2018-08-31 19:02:00 +0530

    changes

--- charts-resources/usage-guide/basic/exporting-as-image-and-pdf-introduction-example-4.js
+++ charts-resources/usage-guide/basic/exporting-as-image-and-pdf-introduction-example-4.js
@@ -121,7 +121,7 @@
                 });
             };
 
-            controls.innerHTML = '<button style="background-color: #6957da; border: none; border-radius: 3px; color: white; padding: 4px 12px; text-align: center; cursor: pointer; outline: none; text-decoration: none; display: inline-block; font-size: 14px;" onClick="exportChart()" >Export chart data</button>';
+            controls.innerHTML = '<button style="background-color: #6957da; border: none; border-radius: 3px; color: white; padding: 4px 12px; text-align: center; cursor: pointer; outline: none; text-decoration: none; display: inline-block; font-size: 14px;" onClick="exportChart()" >Export both Charts in Single PDF</button>';
             controls.style.cssText = 'text-align: center; width: 100%;';
             args.container.appendChild(controls);
         }
