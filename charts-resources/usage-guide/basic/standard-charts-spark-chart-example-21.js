93a1feb645dfcb8be6af38aeb11b333858ed7d99
Dishank Tiwari <dishank@fusioncharts.com>
2018-09-05 11:38:09 +0530

    samples updated

--- charts-resources/usage-guide/basic/standard-charts-spark-chart-example-21.js
+++ charts-resources/usage-guide/basic/standard-charts-spark-chart-example-21.js
@@ -12,78 +12,54 @@
             "subCaptionFontSize": "11",
             "numberPrefix": "$",
             "chartBottomMargin": "20",
-            //Setting period length (Period defined as a set of 3 match)
             "periodlength": "3",
-            //Defining period color
             "periodcolor": "#cccccc",
-            //Setting period transparency
-            "periodalpha": "90",
+            "periodalpha": "90"
         },
         "dataset": [{
             "data": [{
-                    "value": "L"
-                },
-                {
-                    "value": "L"
-                },
-                {
-                    "value": "W"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "W"
-                },
-                {
-                    "value": "W"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "W"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "W"
-                },
-                {
-                    "value": "L"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "W"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "D"
-                },
-                {
-                    "value": "W"
-                }
-            ]
+                "value": "L"
+            }, {
+                "value": "L"
+            }, {
+                "value": "W"
+            }, {
+                "value": "D"
+            }, {
+                "value": "W"
+            }, {
+                "value": "W"
+            }, {
+                "value": "D"
+            }, {
+                "value": "W"
+            }, {
+                "value": "D"
+            }, {
+                "value": "W"
+            }, {
+                "value": "L"
+            }, {
+                "value": "D"
+            }, {
+                "value": "W"
+            }, {
+                "value": "D"
+            }, {
+                "value": "D"
+            }, {
+                "value": "D"
+            }, {
+                "value": "D"
+            }, {
+                "value": "D"
+            }, {
+                "value": "D"
+            }, {
+                "value": "D"
+            }, {
+                "value": "W"
+            }]
         }]
     }
 }
