9cf3955e1571cc4cd9878dc33719376d979d4bb4
DebarghoGhosh <35481853+DebarghoGhosh@users.noreply.github.com>
2018-10-09 13:24:42 +0530

    code changes done

--- charts-resources/usage-guide/basic/chart-configurations-color-range-example-1.js
+++ charts-resources/usage-guide/basic/chart-configurations-color-range-example-1.js
@@ -178,6 +178,96 @@
                         ]
                     }
                 },
+                "maps/world": {
+                    // Map Configuration
+                    "chart": {
+                        "caption": "Average Annual Population Growth",
+                        "subcaption": " 1955-2015",
+                        "numbersuffix": "%",
+                        "includevalueinlabels": "1",
+                        "labelsepchar": ": ",
+                        "entityFillHoverColor": "#FFF9C4",
+                        "theme": "fusion"
+                    },
+                    // Aesthetics; ranges synced with the slider
+                    "colorrange": {
+                        "minvalue": "0",
+                        "code": "#FFE0B2",
+                        "gradient": "1",
+                        "color": [{
+                            "minvalue": "0.5",
+                            "maxvalue": "1.0",
+                            "color": "#FFD74D"
+                        }, {
+                            "minvalue": "1.0",
+                            "maxvalue": "2.0",
+                            "color": "#FB8C00"
+                        }, {
+                            "minvalue": "2.0",
+                            "maxvalue": "3.0",
+                            "color": "#E65100"
+                        }]
+                    },
+                    // Source data as JSON --> id represents countries of world.
+                    "data": [{
+                        "id": "NA",
+                        "value": ".82",
+                        "showLabel": "1"
+                    }, {
+                        "id": "SA",
+                        "value": "2.04",
+                        "showLabel": "1"
+                    }, {
+                        "id": "AS",
+                        "value": "1.78",
+                        "showLabel": "1"
+                    }, {
+                        "id": "EU",
+                        "value": ".40",
+                        "showLabel": "1"
+                    }, {
+                        "id": "AF",
+                        "value": "2.58",
+                        "showLabel": "1"
+                    }, {
+                        "id": "AU",
+                        "value": "1.30",
+                        "showLabel": "1"
+                    }]
+                },
+                "angulargauge": {
+                    // Chart Configuration
+                    "chart": {
+                        "caption": "Nordstorm's Customer Satisfaction Score for 2017",
+                        "lowerLimit": "0",
+                        "upperLimit": "100",
+                        "showValue": "1",
+                        "numberSuffix": "%",
+                        "theme": "fusion",
+                        "showToolTip": "0"
+                    },
+                    // Chart Data
+                    "colorRange": {
+                        "color": [{
+                            "minValue": "0",
+                            "maxValue": "50",
+                            "code": "#F2726F"
+                        }, {
+                            "minValue": "50",
+                            "maxValue": "75",
+                            "code": "#FFC533"
+                        }, {
+                            "minValue": "75",
+                            "maxValue": "100",
+                            "code": "#62B58F"
+                        }]
+                    },
+                    "dials": {
+                        "dial": [{
+                            "value": "81"
+                        }]
+                    }
+                },
             };
 
             e.sender.setJSONData(dataobj.angulargauge);
