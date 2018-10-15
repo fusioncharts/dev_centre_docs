96b6c4fc9cc22f43222cfb00a8b1103ef866387a
Dishank Tiwari <dishank@fusioncharts.com>
2018-10-15 12:41:43 +0530

    fixes

--- charts-resources/usage-guide/basic/standard-charts-drag-able-charts-example-1.js
+++ charts-resources/usage-guide/basic/standard-charts-drag-able-charts-example-1.js
@@ -63,16 +63,17 @@
                 strHTML = "";
             tableCont.setAttribute('id', 'tableCont');
             tableCont.setAttribute('style', 'padding-left: 10px; width: 470px;');
-            strHTML = "<table style='margin: 25px auto;border-collapse: collapse;border: 1px solid;border-bottom: 2px solid #6baa01'><tr><td style='border: 1px solid #fff;padding: 10px 5px;min-width: 70px;text-align: center'></td><th style='" + thStyle + "'>Poultry</th><th style='" + thStyle + "'>Rice</th><th style='" + thStyle + "'>Peanut Butter</th><th style='" + thStyle + "'>Salmon</th><th style='" + thStyle + "'>Cereal</th></tr><tr><td style='background-color: #6baa01;color: #fff; border: 1px solid #6baa01; padding: 10px 5px;min-width: 70px;text-align: center'>Estimated Demand</td><td id='est-0' style='" + tdStyle + "'>19000</td><td id='est-1' style='" + tdStyle + "'>16500</td><td id='est-2' style='" + tdStyle + "'>14300</td><td id='est-3' style='" + tdStyle + "'>10000</td><td id='est-4' style='" + tdStyle + "'>9800</td></tr></table>";
+            strHTML = "<table style='margin: 25px auto;border-collapse: collapse;border: 1px solid;border-bottom: 2px solid #6baa01'><tr><td style='border: 1px solid #fff;padding: 10px 5px;min-width: 70px;text-align: center'></td><th style='" + thStyle + "'>Poultry</th><th style='" + thStyle + "'>Rice</th><th style='" + thStyle + "'>Peanut Butter</th><th style='" + thStyle + "'>Salmon</th><th style='" + thStyle + "'>Cereal</th></tr><tr><td style='background-color: #6baa01;color: #fff; border: 1px solid #6baa01; padding: 10px 5px;min-width: 70px;text-align: center'>Estimated Demand</td><td id='est-1-0' style='" + tdStyle + "'>19000</td><td id='est-1-1' style='" + tdStyle + "'>16500</td><td id='est-1-2' style='" + tdStyle + "'>14300</td><td id='est-1-3' style='" + tdStyle + "'>10000</td><td id='est-1-4' style='" + tdStyle + "'>9800</td></tr></table>";
             tableCont.innerHTML = strHTML;
             args.container.parentNode.insertBefore(tableCont, args.container.nextSibling);
         },
         'dataplotDragEnd': function(evt, arg) {
             debugger;
             var dtIndx = arg && arg.dataIndex,
+                datasetIndex = arg && arg.datasetIndex,
                 val = arg && parseInt(arg.endValue, 10);
 
-            document.getElementById('est-' + dtIndx).innerHTML = val;
+            document.getElementById('est-' + datasetIndex +  '-' +dtIndx).innerHTML = val;
         },
         'dataRestored': function(evtObj) {
             var estimatedOrigValues = ["19000", "16500", "14300", "10000", "9800"],
