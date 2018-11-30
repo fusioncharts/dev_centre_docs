ea1507fa3a1b1eee98be436f4a8ef1e2f379484f
Dishank Tiwari <dishank@fusioncharts.com>
2018-11-30 16:53:36 +0530

    changes

--- charts-resources/fusiontime/getting-started-filter-less/index.js
+++ charts-resources/fusiontime/getting-started-filter-less/index.js
@@ -4,7 +4,7 @@
     width: '95%',
     height: 500,
     dataSource: {
-        data: dataStore.query(FusionCharts.DataStore.Operators.lesser('Sales', 1000)),
+        data: dataStore.query(FusionCharts.DataStore.Operators.less('Sales', 1000)),
         chart: {
         },
         caption: { 
