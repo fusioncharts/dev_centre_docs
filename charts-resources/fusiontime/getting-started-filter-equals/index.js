be750f15c4560f31d859dde10f207c6c95b31750
Dishank Tiwari <dishank@fusioncharts.com>
2018-11-30 18:59:29 +0530

    equals sample

---
+++ charts-resources/fusiontime/getting-started-filter-equals/index.js
@@ -0,0 +1,14 @@
+{
+    type: 'timeseries',
+    renderAt: 'container',
+    width: '95%',
+    height: 500,
+    dataSource: {
+        data: dataStore.query(FusionCharts.DataStore.Operators.equals('Country', 'India')),
+        chart: {
+        },
+        caption: { 
+            text: 'Online Sales of a SuperStore in the US'
+        }
+    }
+}
