0c84440b5d934e03f32e89b0706871511f388fa3
Dishank Tiwari <dishank@fusioncharts.com>
2018-11-30 13:30:35 +0530

    changes in multivariate

--- charts-resources/fusiontime/online-sales-multi-variate/index.js
+++ charts-resources/fusiontime/online-sales-multi-variate/index.js
@@ -1,10 +1,10 @@
 {
     type: 'timeseries',
     renderAt: 'container',
-    width: "95%",
+    width: '95%',
     height: 650,
     dataSource: {
-        data: dataStore.getDataTable(),
+        data: dataStore,
         chart: {},
         caption: {
           text: 'Global Online Sales of a SuperStore'
