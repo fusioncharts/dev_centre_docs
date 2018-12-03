a1740dd9a14e199508cbc6e747425dfb7c4fadb3
Dishank Tiwari <dishank@fusioncharts.com>
2018-12-03 12:04:38 +0530

    sample update for pipe

--- charts-resources/fusiontime/getting-started-filter-pipe/index.js
+++ charts-resources/fusiontime/getting-started-filter-pipe/index.js
@@ -4,7 +4,7 @@
     width: '95%',
     height: 500,
     dataSource: {
-        data: dataStore.query(FusionCharts.DataStore.Operators.equals('Country', 'India')),
+        data: dataStore.query(FusionCharts.DataStore.Operators.pipe(FusionCharts.DataStore.Operators.equals('Country', 'India'), FusionCharts.DataStore.Operators.greater('Sales', 500))),
         chart: {
         },
         caption: { 
