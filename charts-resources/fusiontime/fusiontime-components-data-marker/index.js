4bea707f12c2c5c325c2af0b0ff726e1df93cc9a
Dishank Tiwari <dishank@fusioncharts.com>
2018-12-04 11:02:28 +0530

    changes

--- charts-resources/fusiontime/fusiontime-components-data-marker/index.js
+++ charts-resources/fusiontime/fusiontime-components-data-marker/index.js
@@ -18,69 +18,54 @@
             },
             title: 'Interest Rate'
         }],
-        xAxis: {
-            plot: 'Time',
-            datamarker: [{
-                start: 'Mar-1980',
-                label: 'US inflation peaked at 14.8%.',
-                timeFormat: "%b-%Y",
-                style: {
-                    marker: {
-                        fill: '#D0D6F4'
-                    }
-                }
-            }, {
-                start: 'May-1981',
-                label: 'To control inflation, the Fed started {br} raising interest rates to over {br} 20%.',
-                timeFormat: "%b-%Y"
-            }, {
-                start: 'Jun-1983',
-                label: 'By proactive actions of Mr.Volcker, {br} the inflation falls to 2.4% {br} from the peak of over 14% {br} just three years ago.',
-                timeFormat: "%b-%Y",
-                style: {
-                    marker: {
-                        fill: '#D0D6F4'
-                    }
-                }
-            }, {
-                start: 'Oct-1987',
-                label: 'The Dow Jones Industrial Average lost {br} about 30% of it’s value.',
-                timeFormat: "%b-%Y",
-                style: {
-                    marker: {
-                        fill: '#FBEFCC'
-                    }
-                }
-            }, {
-                start: 'Jan-1989',
-                label: 'George H.W. Bush becomes {br} the 41st president of US!',
-                timeFormat: "%b-%Y"
-            }, {
-                start: 'Aug-1990',
-                label: 'The oil prices spiked to $35 {br} per barrel from $15 per barrel {br} because of the Gulf War.',
-                timeFormat: "%b-%Y"
-            }, {
-                start: 'Dec-1996',
-                label: 'Alan Greenspan warns of the dangers {br} of "irrational exuberance" in financial markets, {br} an admonition that goes unheeded',
-                timeFormat: "%b-%Y"
-            }, {
-                start: 'Sep-2008',
-                label: 'Lehman Brothers collapsed!',
-                timeFormat: "%b-%Y"
-            }, {
-                start: 'Mar-2009',
-                label: 'The net worth of US households {br} stood at a trough of $55 trillion.',
-                timeFormat: "%b-%Y",
-                style: {
-                    marker: {
-                        fill: '#FBEFCC'
-                    }
-                }
-            }, {
-                start: 'Oct-2009',
-                label: 'Unemployment rate peaked {br} in given times to 10%.',
-                timeFormat: "%b-%Y"
-            }]
-        },
+        dataMarker: [{
+            seriesName: "Interest Rate",
+            time: "Mar-1980",
+            identifier: "H",
+            timeFormat: "%b-%Y",
+            tooltext: "As a part of credit control program, under the leadership of Paul Volcker, the Fed tightened the money supply, allowing the federal fund rates to approach 20 percent."
+        }, {
+            seriesName: "Interest Rate",
+            time: "Aug-1982",
+            identifier: "L",
+            timeFormat: "%b-%Y",
+            tooltext: "The FED eases off the monetary brakes, allowing interest rates to fall and the economy to begin a strong recovery."
+        }, {
+            seriesName: "Interest Rate",
+            time: "Oct-1987",
+            identifier: "L",
+            timeFormat: "%b-%Y",
+            tooltext: "The FED is forced to ease rate after the stock market crash."
+        }, {
+            seriesName: "Interest Rate",
+            time: "May-1989",
+            identifier: "H",
+            timeFormat: "%b-%Y",
+            tooltext: "Liquidity problem forced the Fed to increase rate to nearly 10%."
+        }, {
+            seriesName: "Interest Rate",
+            time: "Sept-1992",
+            identifier: "L",
+            timeFormat: "%b-%Y",
+            tooltext: "To fight the jobless economy growth the Fed had to reduce the interest rate to 3%."
+        }, {
+            seriesName: "Interest Rate",
+            time: "Jun-2003",
+            identifier: "L",
+            timeFormat: "%b-%Y",
+            tooltext: "Struggling to revive the economy, the FED cuts it’s benchmark rate to 1%."
+        }, {
+            seriesName: "Interest Rate",
+            time: "Sep-2007",
+            identifier: "L",
+            timeFormat: "%b-%Y",
+            tooltext: "Fed started reducing the Federal Fund Rate."
+        }, {
+            seriesName: "Interest Rate",
+            time: "Dec-2008",
+            identifier: "L",
+            timeFormat: "%b-%Y",
+            tooltext: "Fed reduced the interest rates to sub 0.25% to manage the menace of longest economic downturn since World War 2"
+        }],
     }
 }
