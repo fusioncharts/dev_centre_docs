db0362560376a923383765c27afaa7ad5240b7a7
Dishank Tiwari <dishank@fusioncharts.com>
2018-12-04 11:52:14 +0530

    time marker updated

--- charts-resources/fusiontime/fusiontime-components-time-marker/index.js
+++ charts-resources/fusiontime/fusiontime-components-time-marker/index.js
@@ -5,37 +5,82 @@
     height: 450,
     dataSource: {
         data: dataStore,
-        chart: {
+        caption: {
+            text: 'Interest Rate Analysis'
+        },
+        subCaption: {
+            text: 'Federal Reserve (USA)'
         },
         yAxis: [{
-            "title": "Sales",
+            plot: 'Interest Rate',
+            format: {
+                suffix: '%'
+            },
+            title: 'Interest Rate'
         }],
         xAxis: {
             plot: 'Time',
             timemarker: [{
-                start: 'Jul-1981',
-                end: 'Nov-1982',
-                label: 'Economic downturn was triggered by {br} tight monetary policy in an effort to {br} fight mounting inflation.',
+                start: 'Mar-1980',
+                label: 'US inflation peaked at 14.8%.',
+                timeFormat: "%b-%Y",
+                style: {
+                    marker: {
+                        fill: '#D0D6F4'
+                    }
+                }
+            }, {
+                start: 'May-1981',
+                label: 'To control inflation, the Fed started {br} raising interest rates to over {br} 20%.',
                 timeFormat: "%b-%Y"
             }, {
-                start: 'Jul-1990',
-                end: 'Mar-1991',
-                label: 'This eight month recession period {br} was characterized by a sluggish employment recovery, {br} most commonly referred to as a jobless recovery.',
+                start: 'Jun-1983',
+                label: 'By proactive actions of Mr.Volcker, {br} the inflation falls to 2.4% {br} from the peak of over 14% {br} just three years ago.',
+                timeFormat: "%b-%Y",
+                style: {
+                    marker: {
+                        fill: '#D0D6F4'
+                    }
+                }
+            }, {
+                start: 'Oct-1987',
+                label: 'The Dow Jones Industrial Average lost {br} about 30% of it’s value.',
+                timeFormat: "%b-%Y",
+                style: {
+                    marker: {
+                        fill: '#FBEFCC'
+                    }
+                }
+            }, {
+                start: 'Jan-1989',
+                label: 'George H.W. Bush becomes {br} the 41st president of US!',
                 timeFormat: "%b-%Y"
             }, {
-                start: 'Jun-2004',
-                end: 'Jul-2006',
-                label: 'The Fed after raising interest rates {br} at 17 consecutive meetings, ends its campaign {br} to slow the economy and forestall inflation.',
+                start: 'Aug-1990',
+                label: 'The oil prices spiked to $35 {br} per barrel from $15 per barrel {br} because of the Gulf War.',
                 timeFormat: "%b-%Y"
             }, {
-                start: 'Dec-2007',
-                end: 'Jun-2009',
-                label: 'Recession caused by the worst {br} collapse of financial system in recent {br} times.',
+                start: 'Dec-1996',
+                label: 'Alan Greenspan warns of the dangers {br} of "irrational exuberance" in financial markets, {br} an admonition that goes unheeded',
+                timeFormat: "%b-%Y"
+            }, {
+                start: 'Sep-2008',
+                label: 'Lehman Brothers collapsed!',
+                timeFormat: "%b-%Y"
+            }, {
+                start: 'Mar-2009',
+                label: 'The net worth of US households {br} stood at a trough of $55 trillion.',
+                timeFormat: "%b-%Y",
+                style: {
+                    marker: {
+                        fill: '#FBEFCC'
+                    }
+                }
+            }, {
+                start: 'Oct-2009',
+                label: 'Unemployment rate peaked {br} in given times to 10%.',
                 timeFormat: "%b-%Y"
             }]
         },
-        caption: {
-            text: 'Interest Rate Analysis'
-        }
     }
 }
