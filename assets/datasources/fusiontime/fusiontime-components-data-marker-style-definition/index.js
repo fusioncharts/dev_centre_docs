7b466deec77140b124d4052977376b8d35287617
Dishank Tiwari <dishank@fusioncharts.com>
2019-01-17 16:27:02 +0530

    changes

--- assets/datasources/fusiontime/fusiontime-components-data-marker-style-definition/index.js
+++ assets/datasources/fusiontime/fusiontime-components-data-marker-style-definition/index.js
@@ -31,8 +31,8 @@
             "timeFormat": "%b-%Y",
             "tooltext": "As a part of credit control program, under the leadership of Paul Volcker, the Fed tightened the money supply, allowing the federal fund rates to approach 20 percent."
             "style": {
-                "text": colorstyle,
-                "marker": colorstyle
+                "text": "colorstyle",
+                "marker": "colorstyle"
             }
         }, {
             "seriesName": "Interest Rate",
