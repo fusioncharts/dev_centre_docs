76b9eafbb01608fa6aff77c1ee28bd22fadb73ad
Dishank Tiwari <dishank@fusioncharts.com>
2019-03-06 14:51:28 +0530

    reference line first sample fixed

--- assets/datasources/fusiontime/online-sales-single-series-column-data-plot-reference-line/index.js
+++ assets/datasources/fusiontime/online-sales-single-series-column-data-plot-reference-line/index.js
@@ -10,6 +10,11 @@
         caption: {
             text: 'Online Sales of a SuperStore in the US'
         },
-        
+        yAxis: [{
+            referenceLine: [{
+                label: 'Daily Target',
+                value: '300'
+            }]
+        }],
     }
 }
