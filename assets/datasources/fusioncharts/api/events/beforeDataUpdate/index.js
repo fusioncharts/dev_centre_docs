e095fc3eb380d32a15f5ca513287058f15dc8bd4
Dishank Tiwari <dishank@fusioncharts.com>
2019-06-27 16:26:38 +0530

    beforeDataUpdate page updated

--- assets/datasources/fusioncharts/api/events/beforeDataUpdate/index.js
+++ assets/datasources/fusioncharts/api/events/beforeDataUpdate/index.js
@@ -5,107 +5,99 @@
    height: '350',
    dataFormat: 'json',
    dataSource: {
-      chart: {
-         caption: 'Monthly revenue for last year',
-         subCaption: "Harry's SuperMart",
-         xAxisName: 'Month',
-         yAxisName: 'Revenue (In USD)',
-         numberPrefix: '$',
-         theme: 'fusion',
-      },
-      data: [
-         {
-            label: 'Jan',
-            value: '420000',
-         },
-         {
-            label: 'Feb',
-            value: '810000',
-         },
-         {
-            label: 'Mar',
-            value: '720000',
-         },
-         {
-            label: 'Apr',
-            value: '550000',
-         },
-         {
-            label: 'May',
-            value: '910000',
-         },
-         {
-            label: 'Jun',
-            value: '510000',
-         },
-         {
-            label: 'Jul',
-            value: '680000',
-         },
-         {
-            label: 'Aug',
-            value: '620000',
-         },
-         {
-            label: 'Sep',
-            value: '610000',
-         },
-         {
-            label: 'Oct',
-            value: '490000',
-         },
-         {
-            label: 'Nov',
-            value: '900000',
-         },
-         {
-            label: 'Dec',
-            value: '730000',
-         },
-      ],
+       "chart": {
+           "caption": "Monthly revenue for last year",
+           "subCaption": "Harry's SuperMart",
+           "xAxisName": "Month",
+           "yAxisName": "Revenue (In USD)",
+           "numberPrefix": "$",
+           "paletteColors": "#0075c2",
+           "bgColor": "#ffffff",
+           "borderAlpha": "20",
+           "canvasBorderAlpha": "0",
+           "usePlotGradientColor": "0",
+           "plotBorderAlpha": "10",
+           "placevaluesInside": "1",
+           "rotatevalues": "1",
+           "valueFontColor": "#ffffff",
+           "showXAxisLine": "1",
+           "xAxisLineColor": "#999999",
+           "divlineColor": "#999999",
+           "divLineIsDashed": "1",
+           "showAlternateHGridColor": "0",
+           "subcaptionFontBold": "0",
+           "subcaptionFontSize": "14"
+       },
+       "data": [{
+           "label": "Jan",
+           "value": "420000"
+       }, {
+           "label": "Feb",
+           "value": "810000"
+       }, {
+           "label": "Mar",
+           "value": "720000"
+       }, {
+           "label": "Apr",
+           "value": "550000"
+       }, {
+           "label": "May",
+           "value": "910000"
+       }, {
+           "label": "Jun",
+           "value": "510000"
+       }, {
+           "label": "Jul",
+           "value": "680000"
+       }, {
+           "label": "Aug",
+           "value": "620000"
+       }, {
+           "label": "Sep",
+           "value": "610000"
+       }, {
+           "label": "Oct",
+           "value": "490000"
+       }, {
+           "label": "Nov",
+           "value": "900000"
+       }, {
+           "label": "Dec",
+           "value": "730000"
+       }]
    },
-   events: {
-      /**
-       * @description
-       * This event is triggered just before chart data is passed to the chart.
-       *
-       * @param {Object} eventObj: An object containing all the details related to this event like eventId, sender, etc.
-       * @param {Object} dataObj: An object containing all the details related to chart data, such as the format and source of the chart data.
-       */
+   "events": {
 
-      beforeDataUpdate: function(eventObj, dataObj) {
-         var container = document.getElementById('beforeDataUpdate-example');
+       /**
+        * @description
+        * This event is triggered just before chart data is passed to the chart.
+        *
+        * @param {Object} eventObj: An object containing all the details related to this event like eventId, sender, etc.
+        * @param {Object} dataObj: An object containing all the details related to chart data, such as the format and source of the chart data.
+        */
 
-         function createTable(headers) {
-            var table = document.createElement('table');
-            var tr = document.createElement('tr');
-            headers.forEach(header => {
-               var th = document.createElement('th');
-               th.innerText = header;
-               tr.appendChild(th);
-            });
-            table.appendChild(tr);
-            return table;
-         }
+       "beforeDataUpdate": function(eventObj, dataObj) {
+           console.log(eventObj);
+           var header = document.getElementById('header');
+           header.style.display = 'block';
 
-         function dataPopulate(data) {
-            var tr = document.createElement('tr');
-            data.forEach(d => {
-               var td = document.createElement('td');
-               td.innerText = d;
-               tr.appendChild(td);
-            });
-            return tr;
-         }
+           var tempDiv = document.createElement('div');
+           var attrsTable = document.getElementById('attrs-table');
+           var titleDiv, valueDiv;
+           for (var prop in dataObj) {
+               titleDiv = document.createElement('div');
+               titleDiv.className = 'title';
+               titleDiv.innerHTML = prop;
 
-         var table = createTable(['Parameter Name', 'Parameter Value']);
+               valueDiv = document.createElement('div');
+               valueDiv.className = 'value';
+               valueDiv.innerHTML = dataObj[prop];
 
-         for (var prop in dataObj) {
-            var dataEle = dataPopulate([prop, dataObj[prop]]);
-            table.appendChild(dataEle);
-         }
-
-         container.appendChild(table);
-      }
+               tempDiv.appendChild(titleDiv);
+               tempDiv.appendChild(valueDiv);
+           }
+           attrsTable.innerHTML = '';
+           attrsTable.appendChild(tempDiv);
+       }
    }
 }
