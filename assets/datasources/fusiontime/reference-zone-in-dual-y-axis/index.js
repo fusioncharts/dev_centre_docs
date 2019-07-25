9b25b65ae56dc558c7109989c22c34e4405021a9
Dishank Tiwari <dishank@fusioncharts.com>
2019-07-25 16:42:23 +0530

    sample added

--- assets/datasources/fusiontime/reference-zone-in-dual-y-axis/index.js
+++ assets/datasources/fusiontime/reference-zone-in-dual-y-axis/index.js
@@ -1,62 +1,62 @@
 {
-  type: 'timeseries',
-  renderAt: 'container',
-  width: 95%,
-  height: 450,
-  dataSource: {
-      data: dataStore,
-      chart: {
-          multiCanvas: false
-      },
-      caption: {
-          text: 'Energy & Temperature Measurements'
-      },
-      yAxis: [{
-          plot: [{
-              value: 'Energy',
-              connectNullData: true,
-              type: 'line'
-          }],
-          referenceZone: [{
-              label: 'High energy consumption',
-              valueMin: '170',
-              valueMax: '220',
-          }],
-          format: {
-              suffix: ' kWh'
-          }
-      }, {
-          plot: [{
-              value: 'Temperature',
-              connectNullData: true,
-              type: 'line'
-          }],
-          referenceZone: [{
-              label: 'Cold weather',
-              valueMin: '3',
-              valueMax: '4.5',
-              style: {
-                  marker: {
-                      fill: '#04F5E6',
-                      stroke: '#04F5E6'
-                  },
-                  'marker-text': {
-                      fill: '#000000'
-                  },
-                  'marker:hover': {
-                      fill: '#08DECD'
-                  },
-                  'marker-zone:hover': {
-                      "stroke": "#04F5E6"
-                  },
-                  'marker-notch:hover': {
-                      "stroke": "#04F5E6"
-                  }
-              }
-          }],
-          format: {
-              suffix: ' °C'
-          }
-      }]
-  }
+    type: 'timeseries',
+    renderAt: 'container',
+    width: 800,
+    height: 600,
+    dataSource: {
+        data: dataStore,
+        chart: {
+            multiCanvas: false
+        },
+        caption: {
+            text: 'Energy & Temperature Measurements'
+        },
+        yAxis: [{
+            plot: [{
+                value: 'Energy',
+                connectNullData: true,
+                type: 'line'
+            }],
+            referenceZone: [{
+                label: 'High energy consumption',
+                valueMin: '170',
+                valueMax: '220',
+            }],
+            format: {
+                suffix: ' kWh'
+            }
+        }, {
+            plot: [{
+                value: 'Temperature',
+                connectNullData: true,
+                type: 'line'
+            }],
+            referenceZone: [{
+                label: 'Cold weather',
+                valueMin: '3',
+                valueMax: '4.5',
+                style: {
+                    marker: {
+                        fill: '#04F5E6',
+                        stroke: '#04F5E6'
+                    },
+                    'marker-text': {
+                        fill: '#000000'
+                    },
+                    'marker:hover': {
+                        fill: '#08DECD'
+                    },
+                    'marker-zone:hover': {
+                        "stroke": "#04F5E6"
+                    },
+                    'marker-notch:hover': {
+                        "stroke": "#04F5E6"
+                    }
+                }
+            }],
+            format: {
+                suffix: ' °C'
+            }
+        }]
+    }
 }
