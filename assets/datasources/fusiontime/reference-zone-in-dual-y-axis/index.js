83fe6c047899a19e39f84efc286609a1067411ba
Dishank Tiwari <dishank@fusioncharts.com>
2019-07-25 16:26:12 +0530

    sample added

--- assets/datasources/fusiontime/reference-zone-in-dual-y-axis/index.js
+++ assets/datasources/fusiontime/reference-zone-in-dual-y-axis/index.js
@@ -1,79 +1,62 @@
 {
-    type: 'timeseries',
-    renderAt: 'container',
-    width: 800,
-    height: 600,
-    dataSource: {
-        chart: {
-            multiCanvas: false
-        },
-        caption: {
-            text: 'Energy & Temperature Measurements'
-        },
-        yAxis: [{
-                plot: [{
-                    value: 'Energy',
-                    connectNullData: true,
-                    type: 'line'
-                }],
-                format: {
-                    suffix: ' kWh'
-                },
-            }, {
-                plot: [{
-                    value: 'Temperature',
-                    connectNullData: true,
-                    type: 'line'
-                }],
-                format: {
-                    suffix: ' °C'
-                }
-            },
-            referenceZone: [{
-                label: 'Energy',
-                valueMin: '120',
-                valueMax: '180',
-                style: {
-                    marker: {
-                        fill: '#D2C9FF',
-                        stroke: '#D2C9FF'
-                    },
-                    'marker-text': {
-                        fill: '#000000'
-                    },
-                    'marker:hover': {
-                        fill: '#D2C9FF'
-                    },
-                    'marker-zone:hover': {
-                        "stroke": "#D2C9FF"
-                    },
-                    'marker-notch:hover': {
-                        "stroke": "#D2C9FF"
-                    }
-                }
-            }, {
-                label: 'Temperature',
-                valueMin: '3',
-                valueMax: '5',
-                style: {
-                    marker: {
-                        fill: '#B4F5E6',
-                        stroke: '#B4F5E6'
-                    },
-                    'marker-text': {
-                        fill: '#000000'
-                    },
-                    'marker:hover': {
-                        fill: '#98DECD'
-                    },
-                    'marker-zone:hover': {
-                        "stroke": "#B4F5E6"
-                    },
-                    'marker-notch:hover': {
-                        "stroke": "#B4F5E6"
-                    }
-                }
-            }]
-        ]
-    }
+  type: 'timeseries',
+  renderAt: 'container',
+  width: 95%,
+  height: 450,
+  dataSource: {
+      data: dataStore,
+      chart: {
+          multiCanvas: false
+      },
+      caption: {
+          text: 'Energy & Temperature Measurements'
+      },
+      yAxis: [{
+          plot: [{
+              value: 'Energy',
+              connectNullData: true,
+              type: 'line'
+          }],
+          referenceZone: [{
+              label: 'High energy consumption',
+              valueMin: '170',
+              valueMax: '220',
+          }],
+          format: {
+              suffix: ' kWh'
+          }
+      }, {
+          plot: [{
+              value: 'Temperature',
+              connectNullData: true,
+              type: 'line'
+          }],
+          referenceZone: [{
+              label: 'Cold weather',
+              valueMin: '3',
+              valueMax: '4.5',
+              style: {
+                  marker: {
+                      fill: '#04F5E6',
+                      stroke: '#04F5E6'
+                  },
+                  'marker-text': {
+                      fill: '#000000'
+                  },
+                  'marker:hover': {
+                      fill: '#08DECD'
+                  },
+                  'marker-zone:hover': {
+                      "stroke": "#04F5E6"
+                  },
+                  'marker-notch:hover': {
+                      "stroke": "#04F5E6"
+                  }
+              }
+          }],
+          format: {
+              suffix: ' °C'
+          }
+      }]
+  }
 }
