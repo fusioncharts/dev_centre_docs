cc27cc84e01e2de90fa15c61b38a042f0164a44b
Dishank Tiwari <dishank@fusioncharts.com>
2019-09-02 14:23:30 +0530

    beforeInitialize event updated

--- assets/datasources/fusioncharts/api/events/beforeInitialize/index.js
+++ assets/datasources/fusioncharts/api/events/beforeInitialize/index.js
@@ -1,117 +1,92 @@
 {
    type: 'column2d',
    renderAt: 'chart-container',
-   width: '500',
-   height: '350',
+   width: '600',
+   height: '420',
    dataFormat: 'json',
    dataSource: {
-      chart: {
-         caption: 'Monthly revenue for last year',
-         subCaption: "Harry's SuperMart",
-         xAxisName: 'Month',
-         yAxisName: 'Revenue (In USD)',
-         numberPrefix: '$',
-         placevaluesInside: '1',
-         rotatevalues: '1',
-         divLineIsDashed: '1',
-         showAlternateHGridColor: '0',
-         subcaptionFontBold: '0',
-         subcaptionFontSize: '14',
-         theme: 'fusion'
-      },
-      data: [
-         {
-            label: 'Jan',
-            value: '420000',
-         },
-         {
-            label: 'Feb',
-            value: '810000',
-         },
-         {
-            label: 'Mar',
-            value: '720000',
-         },
-         {
-            label: 'Apr',
-            value: '550000',
-         },
-         {
-            label: 'May',
-            value: '910000',
-         },
-         {
-            label: 'Jun',
-            value: '510000',
-         },
-         {
-            label: 'Jul',
-            value: '680000',
-         },
-         {
-            label: 'Aug',
-            value: '620000',
-         },
-         {
-            label: 'Sep',
-            value: '610000',
-         },
-         {
-            label: 'Oct',
-            value: '490000',
-         },
-         {
-            label: 'Nov',
-            value: '900000',
-         },
-         {
-            label: 'Dec',
-            value: '730000',
-         },
-      ],
+       "chart": {
+           "caption": "Monthly revenue for last year",
+           "subCaption": "Harry's SuperMart",
+           "xAxisName": "Month",
+           "yAxisName": "Revenue (In USD)",
+           "numberPrefix": "$",
+           "showValues": "1",
+           "valueFontColor": "#ffffff",
+           "placevaluesInside": "1",
+           "rotatevalues": "1",
+           "theme": "fusion"
+       },
+       "data": [{
+           "label": "Jan",
+           "value": "420000"
+       }, {
+           "label": "Feb",
+           "value": "810000"
+       }, {
+           "label": "Mar",
+           "value": "720000"
+       }, {
+           "label": "Apr",
+           "value": "550000"
+       }, {
+           "label": "May",
+           "value": "910000"
+       }, {
+           "label": "Jun",
+           "value": "510000"
+       }, {
+           "label": "Jul",
+           "value": "680000"
+       }, {
+           "label": "Aug",
+           "value": "620000"
+       }, {
+           "label": "Sep",
+           "value": "610000"
+       }, {
+           "label": "Oct",
+           "value": "490000"
+       }, {
+           "label": "Nov",
+           "value": "900000"
+       }, {
+           "label": "Dec",
+           "value": "730000"
+       }]
    },
-   events: {
-      /**
-       * @description
-       * The beforeInitialize event is triggered every time a new instance of the FusionCharts object is created.
-       *
-       * @param {Object} eventObj: An object containing all the details related to this event like eventId, sender, etc.
-       * @param {Object} dataObj: An object containing all the details related to the chart, such as the chart type, the format and source of chart data, and so on.
-       */
+   "events": {
 
-      beforeInitialize: function(eventObj, dataObj) {
-         var container = document.getElementById('beforeInitialize-example');
+       /**
+        * @description
+        * The beforeInitialize event is triggered every time a new instance of the FusionCharts object is created.
+        *
+        * @param {Object} eventObj: An object containing all the details related to this event like eventId, sender, etc.
+        * @param {Object} dataObj: An object containing all the details related to the chart, such as the chart type, the format and source of chart data, and so on.
+        */
 
-         function createTable(headers) {
-            var table = document.createElement('table');
-            var tr = document.createElement('tr');
-            headers.forEach(header => {
-               var th = document.createElement('th');
-               th.innerText = header;
-               tr.appendChild(th);
-            });
-            table.appendChild(tr);
-            return table;
-         }
+       "beforeInitialize": function(eventObj, dataObj) {
+           console.log(eventObj);
+           var header = document.getElementById('header');
+           header.style.display = 'block';
 
-         function dataPopulate(data) {
-            var tr = document.createElement('tr');
-            data.forEach(d => {
-               var td = document.createElement('td');
-               td.innerText = d;
-               tr.appendChild(td);
-            });
-            return tr;
-         }
+           var tempDiv = document.createElement('div');
+           var attrsTable = document.getElementById('attrs-table');
+           var titleDiv, valueDiv;
+           for (var prop in dataObj) {
+               titleDiv = document.createElement('div');
+               titleDiv.className = 'title';
+               titleDiv.innerHTML = prop;
 
-         var table = createTable(['Parameter Name', 'Parameter Value']);
+               valueDiv = document.createElement('div');
+               valueDiv.className = 'value';
+               valueDiv.innerHTML = dataObj[prop];
 
-         for (var prop in dataObj) {
-            var dataEle = dataPopulate([prop, dataObj[prop]]);
-            table.appendChild(dataEle);
-         }
-
-         container.appendChild(table);
-      }
+               tempDiv.appendChild(titleDiv);
+               tempDiv.appendChild(valueDiv);
+           }
+           attrsTable.innerHTML = '';
+           attrsTable.appendChild(tempDiv);
+       }
    }
 }
