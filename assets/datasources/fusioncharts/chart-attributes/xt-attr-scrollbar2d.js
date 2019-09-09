0431cecdbd25c5c636e5394911553f4fe01ac2fd
Dishank Tiwari <dishank@fusioncharts.com>
2019-09-09 13:15:52 +0530

    xt-attr-scrollbar2d.js created

---
+++ assets/datasources/fusioncharts/chart-attributes/xt-attr-scrollbar2d.js
@@ -0,0 +1,127 @@
+{
+    type: 'scrollBar2d',
+    renderAt: 'chart-container',
+    width: '100%',
+    height: '100%',
+    dataFormat: 'json',
+    dataSource: {
+        "chart": {
+            "caption": "Sales Trends",
+            "subcaption": "FY 2018 - FY 2019",
+            "xaxisname": "Month",
+            "yaxisname": "Revenue",
+            "showvalues": "1",
+            "numberprefix": "$",
+            "labeldisplay": "WRAP",
+            "linethickness": "3",
+            "numVisiblePlot": "12",
+            "scrollheight": "10",
+            "flatScrollBars": "1",
+            "scrollShowButtons": "0",
+            "scrollColor": "#cccccc",
+            "theme": "fusion"
+        },
+        "categories": [{
+            "category": [{
+                "label": "Jan 2012"
+            }, {
+                "label": "Feb 2012"
+            }, {
+                "label": "Mar 2012"
+            }, {
+                "label": "Apr 2012"
+            }, {
+                "label": "May 2012"
+            }, {
+                "label": "Jun 2012"
+            }, {
+                "label": "Jul 2012"
+            }, {
+                "label": "Aug 2012"
+            }, {
+                "label": "Sep 2012"
+            }, {
+                "label": "Oct 2012"
+            }, {
+                "label": "Nov 2012"
+            }, {
+                "label": "Dec 2012"
+            }, {
+                "label": "Jan 2013"
+            }, {
+                "label": "Feb 2013"
+            }, {
+                "label": "Mar 2013"
+            }, {
+                "label": "Apr 2013"
+            }, {
+                "label": "May 2013"
+            }, {
+                "label": "Jun 2013"
+            }, {
+                "label": "Jul 2013"
+            }, {
+                "label": "Aug 2013"
+            }, {
+                "label": "Sep 2013"
+            }, {
+                "label": "Oct 2013"
+            }, {
+                "label": "Nov 2013"
+            }, {
+                "label": "Dec 2013"
+            }]
+        }],
+        "dataset": [{
+            "data": [{
+                "value": "27400"
+            }, {
+                "value": "29800"
+            }, {
+                "value": "25800"
+            }, {
+                "value": "26800"
+            }, {
+                "value": "29600"
+            }, {
+                "value": "32600"
+            }, {
+                "value": "31800"
+            }, {
+                "value": "36700"
+            }, {
+                "value": "29700"
+            }, {
+                "value": "31900"
+            }, {
+                "value": "34800"
+            }, {
+                "value": "24800"
+            }, {
+                "value": "26300"
+            }, {
+                "value": "31800"
+            }, {
+                "value": "30900"
+            }, {
+                "value": "33000"
+            }, {
+                "value": "36200"
+            }, {
+                "value": "32100"
+            }, {
+                "value": "37500"
+            }, {
+                "value": "38500"
+            }, {
+                "value": "35400"
+            }, {
+                "value": "38200"
+            }, {
+                "value": "33300"
+            }, {
+                "value": "38300"
+            }]
+        }]
+    }
+}
