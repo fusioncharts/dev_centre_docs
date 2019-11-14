d770d04e3e55c8cde6e393515449e711649c0c5a
Dishank Tiwari <dishank@fusioncharts.com>
2019-11-14 18:21:31 +0530

    js file added for sunburst attribute sample

---
+++ assets/datasources/fusioncharts/chart-attributes/xt-attr-sunburst.js
@@ -0,0 +1,105 @@
+{
+    type: 'sunburst',
+    renderAt: 'chart-container',
+    width: '100%',
+    height: '100%',
+    dataFormat: 'json',
+    dataSource: {
+        "chart": {
+            "caption": "Revenue Vs Expense",
+            "subCaption": "Last year",
+            "xaxisname": "Month",
+            "yaxisname": "Amount (In USD)",
+            "numberprefix": "$",
+            "useforwardsteps": "0",
+            "theme": "fusion"
+        },
+        "categories": [{
+            "category": [{
+                "label": "Jan"
+            }, {
+                "label": "Feb"
+            }, {
+                "label": "Mar"
+            }, {
+                "label": "Apr"
+            }, {
+                "label": "May"
+            }, {
+                "label": "Jun"
+            }, {
+                "label": "Jul"
+            }, {
+                "label": "Aug"
+            }, {
+                "label": "Sep"
+            }, {
+                "label": "Oct"
+            }, {
+                "label": "Nov"
+            }, {
+                "label": "Dec"
+            }]
+        }],
+        "dataset": [{
+            "seriesname": "Revenue",
+            "linethickness": "3",
+            "anchorradius": "3",
+            "data": [{
+                "value": "374000"
+            }, {
+                "value": "350000"
+            }, {
+                "value": "380000"
+            }, {
+                "value": "340000"
+            }, {
+                "value": "398000"
+            }, {
+                "value": "326000"
+            }, {
+                "value": "448000"
+            }, {
+                "value": "379000"
+            }, {
+                "value": "355000"
+            }, {
+                "value": "374000"
+            }, {
+                "value": "348000"
+            }, {
+                "value": "402000"
+            }]
+        }, {
+            "seriesname": "Expense",
+            "linethickness": "3",
+            "anchorradius": "3",
+            "data": [{
+                "value": "100000"
+            }, {
+                "value": "115000"
+            }, {
+                "value": "135000"
+            }, {
+                "value": "150000"
+            }, {
+                "value": "110000"
+            }, {
+                "value": "98000"
+            }, {
+                "value": "118000"
+            }, {
+                "value": "197000"
+            }, {
+                "value": "228000"
+            }, {
+                "value": "249000"
+            }, {
+                "value": "229000"
+            }, {
+                "value": "208000"
+            }]
+        }]
+    }
+
+}
