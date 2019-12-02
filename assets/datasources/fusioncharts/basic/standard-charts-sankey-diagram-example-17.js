d048dcad1dcd838709964b259e5fef0bafa4df2a
Dishank Tiwari <dishank@fusioncharts.com>
2019-12-02 14:15:25 +0530

    sankey sample updated

--- assets/datasources/fusioncharts/basic/standard-charts-sankey-diagram-example-17.js
+++ assets/datasources/fusioncharts/basic/standard-charts-sankey-diagram-example-17.js
@@ -1,179 +1,355 @@
 {
     type: "sankey",
-    renderAt: "container",
+    renderAt: "sankey-container",
     id: "sankey-chart",
     width: "700",
-    height: "450",
+    height: "500",
     dataFormat: "json",
     dataSource: {
         chart: {
-            caption: "Immigrant Flow for Last Month (In Millions)",
-            legendPosition: "bottom",
-            theme: "fusion"
+            caption: "Interconnecting Flights across Countries, 2019",
+            subcaption: "A Horizontal Sankey with options to configure the node attributes",
+            theme: "fusion",
+            orientation: "horizontal",
+            linkAlpha: 30,
+            linkHoverAlpha: 60,
+            nodeLabelPosition: "start"
         },
         nodes: [{
-                label: "Netherlands"
+                label: "Brazil",
+                nodeWidth: 10
             },
             {
-                label: "Canada"
+                label: "Canada",
+                nodeWidth: 10
             },
             {
-                label: "Belgium"
+                label: "Mexico",
+                nodeWidth: 10
             },
             {
-                label: "Italy"
+                label: "USA",
+                nodeWidth: 10
             },
             {
-                label: "Mexico"
+                label: "Portugal",
+                nodeWidth: 10
             },
             {
-                label: "Russia"
+                label: "France",
+                nodeWidth: 10
             },
             {
-                label: "Spain"
+                label: "Spain",
+                nodeWidth: 10
             },
             {
-                label: "South Korea"
+                label: "England",
+                nodeWidth: 10
             },
             {
-                label: "Germany"
+                label: "South Africa",
+                nodeWidth: 10
             },
             {
-                label: "China"
+                label: "Angola",
+                nodeWidth: 10
             },
             {
-                label: "European Union"
+                label: "Senegal",
+                nodeWidth: 10
             },
             {
-                label: "Japan"
+                label: "Mali",
+                nodeWidth: 10
             },
             {
-                label: "United Kingdom"
-            },
-            {
-                label: "United States"
-            },
-            {
-                label: "France"
-            },
-            {
-                label: "Hong Kong"
-            },
-            {
-                label: "Switzerland"
-            },
-            {
-                label: "Austria"
-            },
-            {
-                label: "Sweden"
+                label: "Morocco",
+                nodeWidth: 10
             }
         ],
         links: [{
-                from: "Netherlands",
-                to: "European Union",
-                value: 798744
+                from: "Brazil",
+                to: "Portugal",
+                value: 5
             },
             {
-                from: "Germany",
-                to: "European Union",
-                value: 1468990
-            },
-            {
-                from: "European Union",
+                from: "Brazil",
                 to: "France",
-                value: 745931
+                value: 1
             },
             {
-                from: "European Union",
-                to: "United States",
-                value: 660541
+                from: "Brazil",
+                to: "Spain",
+                value: 1
+            },
+            {
+                from: "Brazil",
+                to: "England",
+                value: 1
             },
             {
                 from: "Canada",
-                to: "United States",
-                value: 594546
+                to: "Portugal",
+                value: 1
             },
             {
-                from: "Belgium",
-                to: "European Union",
-                value: 628796
+                from: "Canada",
+                to: "France",
+                value: 5
             },
             {
-                from: "China",
-                to: "Hong Kong",
-                value: 400571
-            },
-            {
-                from: "China",
-                to: "United States",
-                value: 526454
-            },
-            {
-                from: "European Union",
-                to: "United Kingdom",
-                value: 520318
-            },
-            {
-                from: "China",
-                to: "European Union",
-                value: 560536
-            },
-            {
-                from: "Italy",
-                to: "European Union",
-                value: 539556
+                from: "Canada",
+                to: "England",
+                value: 1
             },
             {
                 from: "Mexico",
-                to: "United States",
-                value: 492715
+                to: "Portugal",
+                value: 1
             },
             {
-                from: "Russia",
-                to: "European Union",
-                value: 385778
+                from: "Mexico",
+                to: "France",
+                value: 1
+            },
+            {
+                from: "Mexico",
+                to: "Spain",
+                value: 5
+            },
+            {
+                from: "Mexico",
+                to: "England",
+                value: 1
+            },
+            {
+                from: "USA",
+                to: "Portugal",
+                value: 1
+            },
+            {
+                from: "USA",
+                to: "France",
+                value: 1
+            },
+            {
+                from: "USA",
+                to: "Spain",
+                value: 1
+            },
+            {
+                from: "USA",
+                to: "England",
+                value: 5
+            },
+            {
+                from: "Portugal",
+                to: "Angola",
+                value: 2
+            },
+            {
+                from: "Portugal",
+                to: "Senegal",
+                value: 1
+            },
+            {
+                from: "Portugal",
+                to: "Morocco",
+                value: 1
+            },
+            {
+                from: "Portugal",
+                to: "South Africa",
+                value: 3
+            },
+            {
+                from: "France",
+                to: "Angola",
+                value: 1
+            },
+            {
+                from: "France",
+                to: "Senegal",
+                value: 3
+            },
+            {
+                from: "France",
+                to: "Mali",
+                value: 3
+            },
+            {
+                from: "France",
+                to: "Morocco",
+                value: 3
+            },
+            {
+                from: "France",
+                to: "South Africa",
+                value: 1
             },
             {
                 from: "Spain",
-                to: "European Union",
-                value: 365191
+                to: "Senegal",
+                value: 1
             },
             {
-                from: "China",
-                to: "Japan",
-                value: 312062
+                from: "Spain",
+                to: "Morocco",
+                value: 3
             },
             {
-                from: "European Union",
-                to: "Switzerland",
-                value: 328609
+                from: "Spain",
+                to: "South Africa",
+                value: 1
             },
             {
-                from: "South Korea",
+                from: "England",
+                to: "Angola",
+                value: 1
+            },
+            {
+                from: "England",
+                to: "Senegal",
+                value: 1
+            },
+            {
+                from: "England",
+                to: "Morocco",
+                value: 2
+            },
+            {
+                from: "England",
+                to: "South Africa",
+                value: 7
+            },
+            {
+                from: "South Africa",
                 to: "China",
-                value: 229073
+                value: 5
             },
             {
-                from: "European Union",
-                to: "Austria",
-                value: 244913
+                from: "South Africa",
+                to: "India",
+                value: 1
             },
             {
-                from: "Japan",
-                to: "United States",
-                value: 206091
+                from: "South Africa",
+                to: "Japan",
+                value: 3
             },
             {
-                from: "European Union",
-                to: "Sweden",
-                value: 204849
+                from: "Angola",
+                to: "China",
+                value: 5
             },
             {
-                from: "Germany",
-                to: "United States",
-                value: 184287
+                from: "Angola",
+                to: "India",
+                value: 1
+            },
+            {
+                from: "Angola",
+                to: "Japan",
+                value: 3
+            },
+            {
+                from: "Senegal",
+                to: "China",
+                value: 5
+            },
+            {
+                from: "Senegal",
+                to: "India",
+                value: 1
+            },
+            {
+                from: "Senegal",
+                to: "Japan",
+                value: 3
+            },
+            {
+                from: "Mali",
+                to: "China",
+                value: 5
+            },
+            {
+                from: "Mali",
+                to: "India",
+                value: 1
+            },
+            {
+                from: "Mali",
+                to: "Japan",
+                value: 3
+            },
+            {
+                from: "Morocco",
+                to: "China",
+                value: 5
+            },
+            {
+                from: "Morocco",
+                to: "India",
+                value: 1
+            },
+            {
+                from: "Morocco",
+                to: "Japan",
+                value: 3
             }
         ]
+    },
+    events: {
+        beforeRender: function(eventObj, dataObj) {
+            var chartRef = eventObj.sender;
+            var chartContainer = dataObj.container;
+
+            var radioInputsWrapper = document.createElement('div');
+
+            var eventHandler = function(attr, val) {
+                console.log(attr, val);
+                chartRef.setChartAttribute(attr, val);
+
+            };
+
+            var createRadioNode = function(list) {
+                list.forEach(item => {
+                    var rad = document.createElement("input");
+                    rad.type = "radio";
+                    rad.name = "node";
+                    rad.value = item.value;
+                    rad.id = item.value;
+
+                    rad.onclick = eventHandler.bind(this, item.attr, item.value);
+                    var label = document.createElement('label');
+                    label.style.color = "#000";
+                    label.innerText = item.label;
+
+                    var div = document.createElement('div');
+                    div.style.display = "inlineBlock";
+                    //  div.style.marginRight = "5px";
+                    div.appendChild(rad);
+                    div.appendChild(label);
+
+                    radioInputsWrapper.appendChild(div);
+                })
+
+            }
+
+            var listItems = [{
+                label: "Label one",
+                value: '#ff0000',
+                attr: 'nodeLabelBgColor'
+            }, {
+                label: "Label two",
+                value: 30,
+                attr: 'nodeLabelAlpha'
+            }];
+
+            createRadioNode(listItems);
+
+            chartContainer.appendChild(radioInputsWrapper);
+        }
     }
 }
