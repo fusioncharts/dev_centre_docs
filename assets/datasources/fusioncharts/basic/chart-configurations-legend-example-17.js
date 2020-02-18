63966f0ba5620a1623fd77c928c6e71ea4ca7ec8
Dishank Tiwari <dishank@fusioncharts.com>
2020-02-18 11:50:28 +0530

    samples added for legendposition

---
+++ assets/datasources/fusioncharts/basic/chart-configurations-legend-example-17.js
@@ -0,0 +1,56 @@
+{
+    type: 'mscolumn2d',
+    renderAt: 'chart-container',
+    width: '700',
+    height: '420',
+    dataFormat: 'json',
+    dataSource: {
+        "chart": {
+            "caption": "Comparison of Quarterly Revenue",
+            "subCaption": "Last year vs This year",
+            "xAxisname": "Quarter",
+            "yAxisName": "Revenue (In USD)",
+            "numberPrefix": "$",
+            "showLegend": "1",
+            "legendPosition": "absolute",
+            "legendXPosition": "400",
+            "legendYPosition": "70",
+            "interactiveLegend": "0",
+            "theme": "fusion"
+        },
+        "categories": [{
+            "category": [{
+                "label": "Q1"
+            }, {
+                "label": "Q2"
+            }, {
+                "label": "Q3"
+            }, {
+                "label": "Q4"
+            }]
+        }],
+        "dataset": [{
+            "seriesname": "Last Year",
+            "data": [{
+                "value": "10000"
+            }, {
+                "value": "11500"
+            }, {
+                "value": "12500"
+            }, {
+                "value": "15000"
+            }]
+        }, {
+            "seriesname": "This Year",
+            "data": [{
+                "value": "25400"
+            }, {
+                "value": "29800"
+            }, {
+                "value": "21800"
+            }, {
+                "value": "26800"
+            }]
+        }]
+    }
+}
