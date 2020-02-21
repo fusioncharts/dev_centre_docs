7154b93627e9a3d2939ff269fe1d8cd7cb5e1b8b
Dishank Tiwari <dishank@fusioncharts.com>
2020-02-21 16:57:09 +0530

    samples added for column and area range

--- assets/datasources/fusiontime/examples/area-range-series-chart/index.js
+++ assets/datasources/fusiontime/examples/area-range-series-chart/index.js
@@ -1,22 +1,56 @@
 {
     type: "timeseries",
-    renderAt: "container",
-    width: 680,
-    height: 500,
+    renderAt: "chart-container",
+    width: "100%",
+    height: 470,
     dataSource: {
-        data: dataStore,
-        caption: {
-            text: "Temperature readings of an Italian Town"
+      data: dataStore,
+      caption: {
+        text: "Plot for temperature change",
+        "style": {
+          "text": {
+            "fill": "#ff0000",
+            "font-size": "24"
+          }
+        }
+      },
+      chart: {
+        showLegend: 1,
+        theme: "umber",
+        showtooltip: 1,
+        palettecolors: "#138D75"
+      },
+      yAxis: [{
+        plot: [{
+          value: {
+            high: 'High',
+            low: 'Low'
+          },
+          type: "area-range",
+          name: "India",
+          style: {
+            'fill-opacity': 0.6,
+            'stroke-opacity': 0.95
+          }
+        }],
+        format: {
+          suffix: " C"
+        }
+      }],
+      tooltip: {
+        enabled: "true", // Disables the Tooltip
+        outputTimeFormat: {
+          day: "%d/%m/%y (%a)"
         },
-        yAxis: [{
-            "plot": "Temperature",
-            "title": "Temperature",
-            "plotType": "column",
-            "format": {
-                "suffix": "°C",
-                "label": "Controlled Temperature",
-                "value": "10",
-            }
-        }]
+        style: {
+          container: {
+            "border-color": "#000000",
+            "background-color": "#808B96"
+          },
+          text: {
+            "color": "#FFFFFF"
+          }
+        }
+      }
     }
-}
+  }
--- assets/datasources/fusiontime/examples/column-range-series-chart/index.js
+++ assets/datasources/fusiontime/examples/column-range-series-chart/index.js
@@ -1,22 +1,56 @@
 {
     type: "timeseries",
-    renderAt: "container",
-    width: 680,
-    height: 500,
+    renderAt: "chart-container2",
+    width: "100%",
+    height: 470,
     dataSource: {
-        data: dataStore,
-        caption: {
-            text: "Temperature readings of an Italian Town"
+      data: dataStore,
+      caption: {
+        text: "Plot for Energy Consumption",
+        "style": {
+          "text": {
+            "fill": "#ff0000",
+            "font-size": "24"
+          }
+        }
+      },
+      chart: {
+        showLegend: 1,
+        theme: "candy",
+        showtooltip: 1,
+        palettecolors: "#02ECD0"
+      },
+      yAxis: [{
+        plot: [{
+          value: {
+            high: 'Coal',
+            low: 'Hydro'
+          },
+          type: "column-range",
+          name: "World",
+          style: {
+            'fill-opacity': 0.6,
+            'stroke-opacity': 0.95
+          }
+        }],
+        format: {
+          suffix: "KW"
+        }
+      }],
+      tooltip: {
+        enabled: "true", // Disables the Tooltip
+        outputTimeFormat: {
+          day: "%d/%m/%y (%a)"
         },
-        yAxis: [{
-            "plot": "Temperature",
-            "title": "Temperature",
-            "plotType": "column",
-            "format": {
-                "suffix": "°C",
-                "label": "Controlled Temperature",
-                "value": "10",
-            }
-        }]
+        style: {
+          container: {
+            "border-color": "#000000",
+            "_background-color": "#F2F6F6"
+          },
+          text: {
+            "color": "#FFFFFF"
+          }
+        }
+      }
     }
-}
+  }
