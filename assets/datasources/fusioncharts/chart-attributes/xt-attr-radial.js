e07a17dc4ec858a887767ced9ad26a11c419f4fb
rodriv6 <114236635+rodriv6@users.noreply.github.com>
2024-11-28 12:21:29 -0400

    Create xt-attr-radial.js

---
+++ assets/datasources/fusioncharts/chart-attributes/xt-attr-radial.js
@@ -0,0 +1,41 @@
+{
+  type: 'radialbar',
+  renderAt: 'chart-container',
+  width: '100%',
+  height: '100%',
+  dataFormat: 'json',
+  dataSource: {
+    "chart": {
+      "caption": "Top 5 Android OS Market Share",
+      "subCaption": "January 2021",
+      "showLegend": 1,
+      "innerRadius": 30,
+      "outerRadius": 155,
+      "showLabels": 1,
+      "labelText": "<b>$label</b>",
+      "theme": "fusion"
+    },
+    "data": [
+      {
+        "label": "Nougat 7.0",
+        "value": 4.09
+      },
+      {
+        "label": "Oreo 8.0",
+        "value": 5.19
+      },
+      {
+        "label": "Marshmallow 6.0",
+        "value": 5.77
+      },
+      {
+        "label": "Oreo 8.1",
+        "value": 9.89
+      },
+      {
+        "label": "Pie 9.0",
+        "value": 21.53
+      }
+    ]
+  }
+}
